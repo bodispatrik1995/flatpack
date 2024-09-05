48a6362fd279d2056d05292cd951b302a74ac5d3
Attila112 <gonda.attila112@gmail.com>
2024-09-05 15:18:52 +0200

    almost ready the upload form

--- backend/app/Http/Controllers/PropertyController.php
+++ backend/app/Http/Controllers/PropertyController.php
@@ -12,13 +12,14 @@ class PropertyController extends Controller
     {
         try {
 
-        $properties = Property::all();
+            $properties = Property::all();
 
             return response()->json($properties, 200);
         } catch (\Exception $e) {
             return response()->json(["error" => $e->getMessage()], 500);
         }
     }
+
     public function searchProperty(Request $request)
     {
         try {
@@ -46,8 +47,8 @@ class PropertyController extends Controller
             if ($description) {
                 $query->where('description', 'like', '%' . $description . '%');
             }
-            if ($size){
-                $query->where('size', '=', $size );
+            if ($size) {
+                $query->where('size', '=', $size);
             }
             if ($city) {
                 $query->where('city', 'like', '%' . $city . '%');
@@ -112,50 +113,53 @@ class PropertyController extends Controller
         }
     }
 
-  public function addProperty(Request $request)
-  {
-      try{
-          $id =\auth('sanctum')->user()->id;
-            var_dump($request->houser_number);
-          $property = Property::create([
-              'title'=>$request->title,
-              'user_id'=>$id,
-              'description'=>$request->description,
-              'size'=>$request->size,
-              'city'=>$request->city,
-              'street'=>$request->street,
-              'house_number'=>$request->house_number,
-              'rooms'=>$request->rooms,
-              'bathroom_count'=>$request->bathroom_count,
-              'floor'=>$request->floor,
-              'building_material'=>$request->building_material,
-              'type'=>$request->type,
-              'plot_size'=>$request->plot_size,
-              'garage'=>$request->garage,
-              'facing'=>$request->facing,
-              'price'=>$request->price,
-          ]);
+    public function addProperty(Request $request)
+    {
+        try {
+            $id = \auth('sanctum')->user()->id;
+//            var_dump($request->house_number);
 
+          $property = Property::create([
+              'title' => $request->title,
+              'user_id' => $id,
+              'description' => $request->description,
+              'size' => $request->size,
+              'city' => $request->city,
+              'street' => $request->street,
+              'house_number' => $request->house_number,
+              'rooms' => $request->rooms,
+              'bathroom_count' => $request->bathroom_count,
+              'floor' => $request->floor,
+              'building_material' => $request->building_material,
+              'type' => $request->type,
+              'plot_size' => $request->plot_size,
+              'garage' => $request->garage,
+              'facing' => $request->facing,
+              'price' => $request->price,
+          ]);
+//    var_dump($property->id);
           return response()->json([
               'status' => true,
               'message' => 'Property added successfully',
-              $property
-          ], 200);
-      }catch (\Exception $e){
-          return response()->json(["error" => $e->getMessage()], 500);
-      }
-  }
+              'propertyId' => $property->id,
+              $property,
 
-public function deleteProperty ($request)
-{
-    try{
-        $id = $request->id;
-        Property::where('id', $id)->delete();
-        return response()->json(['message' => 'Property deleted successfully']);
-    }catch(Exception $e){
-        return response()->json(["error" => $e->getMessage()], 500);
+          ], 200);
+      } catch (\Exception $e) {
+            return response()->json(["error" => $e->getMessage()], 500);
+        }
     }
 
-}
+    public function deleteProperty($request)
+    {
+        try {
+            $id = $request->id;
+            Property::where('id', $id)->delete();
+            return response()->json(['message' => 'Property deleted successfully']);
+        } catch (Exception $e) {
+            return response()->json(["error" => $e->getMessage()], 500);
+        }
+
+    }
 
 }
--- frontend/src/Components/Property/UploadFrom.jsx
+++ frontend/src/Components/Property/UploadFrom.jsx
@@ -1,53 +1,164 @@
-import React from 'react';
+import {useState} from "react";
 import Input from "../Searchbar/Input.jsx";
 
 function UploadFrom(props) {
-    // console.log(localStorage.getItem('userToken'))
-    const id = localStorage.getItem('userId')
-    console.log(id)
-    function handleChange (){
+    const token = localStorage.getItem('userToken')
+    const [propertyId, setPropertyId] = useState(null)
+    const [imageValues, setImageValues] = useState({
+        image_path: null,
+        name: "name",
+        property_id: propertyId
+
+    })
+    const [formValues, setFormValues] = useState({
+        title: '',
+        description: '',
+        size: '',
+        city: '',
+        street: '',
+        house_number: '',
+        rooms: '',
+        bathroom_count: '',
+        floor: '',
+        building_material: '',
+        type: '',
+        plot_size: '',
+        garage: false,
+        facing: '',
+        price: '',
+        // addImage: null,
+    });
+
+    async function fetchPropertyImg  () {
+        const response = await fetch('http://127.0.0.1:8000/api/upload_image', {
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: imageValues
+        })
+        const data = await response.json()
+        console.log(data)
     }
+
+    const id = localStorage.getItem('userId');
+    console.log(id);
+
+    function handleChangeImg(e) {
+        const {name, files}= e.target
+        setImageValues( prevState => ({
+            ...prevState,
+                [name]: files[0]
+        }))
+    }
+
+    const handleChange = (event) => {
+        const {name, value, type, checked, files} = event.target;
+
+        setFormValues(prevValues => ({
+            ...prevValues,
+            [name]: type === 'checkbox' ? checked : type === 'file' ? files[0] : value,
+        }));
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await fetch('http://127.0.0.1:8000/api/add/property', {
+            method: "POST",
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            // credentials: 'include', // Important for sending cookies with the request
+            body: JSON.stringify(formValues),
+        })
+        const data =await response.json()
+        console.log(data.propertyId)
+        if (response.ok){
+            console.log("Upload a property")
+            console.log(data)
+        } else {
+            console.log("error")
+            console.log(data)
+        }
+        setPropertyId(data.propertyId);
+        fetchPropertyImg()
+
+    };
+
+    console.log(imageValues)
     return (
         <div>
-            <h1>This is wher you can upload a new property!</h1>
-            <form>
+            <h1>This is where you can upload a new property!</h1>
+            <form onSubmit={handleSubmit}>
                 <label>Title:
-                <Input name={'title'} value={""} type={"text"} placeholder={"Pls write a title"} onChange={handleChange()}/> </label>
+                    <Input name={'title'} value={formValues.title} type={"text"} placeholder={"Pls write a title"}
+                           onChange={handleChange}/>
+                </label>
                 <label>Description:
-                    <Input name={'description'} value={""} type={"text"} placeholder={"Pls write a description"} onChange={handleChange()}/> </label>
+                    <Input name={'description'} value={formValues.description} type={"text"}
+                           placeholder={"Pls write a description"} onChange={handleChange}/>
+                </label>
                 <label>Size:
-                    <Input name={'size'} value={""} type={"number"} placeholder={"Pls write a size"} onChange={handleChange()}/> </label>
+                    <Input name={'size'} value={formValues.size} type={"number"} placeholder={"Pls write a size"}
+                           onChange={handleChange}/>
+                </label>
                 <h2>Address:</h2>
                 <label>City:
-                    <Input name={'city'} value={""} type={"text"} placeholder={"Pls write a city"} onChange={handleChange()}/> </label>
+                    <Input name={'city'} value={formValues.city} type={"text"} placeholder={"Pls write a city"}
+                           onChange={handleChange}/>
+                </label>
                 <label>Street:
-                    <Input name={'street'} value={""} type={"text"} placeholder={"Pls write a street"} onChange={handleChange()}/> </label>
+                    <Input name={'street'} value={formValues.street} type={"text"} placeholder={"Pls write a street"}
+                           onChange={handleChange}/>
+                </label>
                 <label>House Number:
-                    <Input name={'house_number'} value={""} type={"number"} placeholder={"Pls write a number"} onChange={handleChange()}/> </label>
+                    <Input name={'house_number'} value={formValues.house_number} type={"number"}
+                           placeholder={"Pls write a number"} onChange={handleChange}/>
+                </label>
                 <label>Number of Rooms:
-                    <Input name={'rooms'} value={""} type={"number"} placeholder={"Pls write a number"} onChange={handleChange()}/> </label>
+                    <Input name={'rooms'} value={formValues.rooms} type={"number"} placeholder={"Pls write a number"}
+                           onChange={handleChange}/>
+                </label>
                 <label>Number of bathrooms:
-                    <Input name={'bathroom_count'} value={""} type={"number"} placeholder={"Pls write a number"} onChange={handleChange()}/> </label>
+                    <Input name={'bathroom_count'} value={formValues.bathroom_count} type={"number"}
+                           placeholder={"Pls write a number"} onChange={handleChange}/>
+                </label>
                 <label>Floor number:
-                    <Input name={'floor'} value={""} type={"number"} placeholder={"Pls write a number"} onChange={handleChange()}/> </label>
+                    <Input name={'floor'} value={formValues.floor} type={"number"} placeholder={"Pls write a number"}
+                           onChange={handleChange}/>
+                </label>
                 <label>Building material:
-                    <Input name={'building_material'} value={""} type={"text"} placeholder={"Pls write a material"} onChange={handleChange()}/> </label>
+                    <Input name={'building_material'} value={formValues.building_material} type={"text"}
+                           placeholder={"Pls write a material"} onChange={handleChange}/>
+                </label>
                 <label>Type:
-                    <Input name={'type'} value={""} type={"text"} placeholder={"Pls write a type"} onChange={handleChange()}/> </label>
+                    <Input name={'type'} value={formValues.type} type={"text"} placeholder={"Pls write a type"}
+                           onChange={handleChange}/>
+                </label>
                 <label>Plot Size:
-                    <Input name={'plot_size'} value={""} type={"number"} placeholder={"Pls write a number"} onChange={handleChange()}/> </label>
+                    <Input name={'plot_size'} value={formValues.plot_size} type={"number"}
+                           placeholder={"Pls write a number"} onChange={handleChange}/>
+                </label>
                 <label>Garage:
-                    <Input name={'garage'} value={""} type={"checkbox"} placeholder={"Pls write a title"} onChange={handleChange()}/> </label>
+                    <Input name={'garage'} checked={formValues.garage} type={"checkbox"} onChange={handleChange}/>
+                </label>
                 <label>Facing:
-                    <Input name={'facing'} value={""} type={"text"} placeholder={"Pls write a facing"} onChange={handleChange()}/> </label>
+                    <Input name={'facing'} value={formValues.facing} type={"text"} placeholder={"Pls write a facing"}
+                           onChange={handleChange}/>
+                </label>
                 <label>Price:
-                    <Input name={'price'} value={""} type={"number"} placeholder={"Pls write a number"} onChange={handleChange}/> </label>
+                    <Input name={'price'} value={formValues.price} type={"number"} placeholder={"Pls write a number"}
+                           onChange={handleChange}/>
+                </label>
                 <label>Upload pictures:
-                <input type={'file'} name={'addImage'} onChange={handleChange}/></label>
+                    <input type={'file'} name={'image_path'} onChange={handleChangeImg}/>
+                </label>
                 <input type={'submit'}/>
             </form>
-        </div>
-    );
+        </div>)
+
 }
 
 export default UploadFrom;
