a11d1f68eece1c84537bad0e0bfa4ad091b25de3
Bodis Patrik <bodis.uk@gmail.com>
2024-09-05 19:02:33 +0200

    merge

--- frontend/src/Components/PropertyList/PropertyBox.jsx
+++ frontend/src/Components/PropertyList/PropertyBox.jsx
@@ -22,9 +22,10 @@ function PropertyBox(props) {
         }
     };
 
+
     useEffect(() => {
         imgUrl();
-    }, []);
+    }, [props.id]);
 
     return (
         <div className={'max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'} key={props.uniqueKey}>
