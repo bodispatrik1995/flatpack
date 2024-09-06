c6ba377640e84f043350692b6ddc5f21e3011878
Bodis Patrik <bodis.uk@gmail.com>
2024-09-06 10:12:54 +0200

    merge

--- frontend/src/Components/PropertyList/PropertyBox.jsx
+++ frontend/src/Components/PropertyList/PropertyBox.jsx
@@ -5,12 +5,12 @@ function PropertyBox(props) {
     const [loading, setLoading] = useState(false); // State to handle loading
     const [error, setError] = useState(null); // State to handle errors
 
-    // const imgUrl = async () => {
-    //     try {
-    //         const response = await fetch(`http://127.0.0.1:8000/api/image/${props.id}`);
-    //         const data = await response.json();
-    //
-    //         if (response.ok && data.success) {
+     const imgUrl = async () => {
+         try {
+             const response = await fetch(`http://127.0.0.1:8000/api/image/${props.id}`);
+             const data = await response.json();
+
+    /        if (response.ok && data.success) {
     //             setImg(`http://localhost:8000/${data.image}`);
     //         } else {
     //             setImg('https://kep.cdn.indexvas.hu/1/0/2098/20980/209807/20980782_1335884_a378a0520a6450953bd777efc34d0fe3_wm.jpg')
