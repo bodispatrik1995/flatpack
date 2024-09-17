ab42bb21d4bf239141235471d6e12d91fc4c730b
MoorAdam <moor.adam.is@gmail.com>
2024-09-17 13:55:45 +0200

    created basic PropertyCard.jsx frames

--- frontend/src/Components/Property/PropertyCard.jsx
+++ frontend/src/Components/Property/PropertyCard.jsx
@@ -22,7 +22,81 @@ function PropertyCard(props) {
         return <Loading/>
     } else {
         return (
-            <div
+
+            /*<div className={"grid gap-4 grid-cols-3 grid-rows-4"}>
+                <div className={"apply-square-background row-span-3 col-span-2"}></div>
+                <div className={"apply-square-background row-span-2"}></div>
+                <div className={"apply-square-background"}></div>
+
+
+                <div className={"apply-square-background col-span-2"}></div>
+                <div className={"apply-square-background col-span-1"}></div>
+            </div>*/
+
+            <div className={"property-card"}>
+                <div className={"grid gap-4 grid-cols-3 grid-rows-3 Property-headline"}>
+                    <div className={"row-span-3 col-span-2 property-images apply-square-background"}>
+                        <h1>Property images</h1>
+                    </div>
+                    <div className={"row-span-2 property-quick-data apply-square-background"}>
+                        <h1>{property.title}</h1>
+                        <h1>Price: {property.price}$</h1>
+                        <h1>{property.size}m2</h1>
+                    </div>
+                    <div className={"property-quick-actions apply-square-background"}>
+                        <button className={"button"}>Buy</button>
+                        <button className={"button"}>Contact Owner</button>
+                    </div>
+                    <div className={"col-span-2 property-data apply-square-background"}>
+                        <h1>Property data</h1>
+                        <table className={"table-auto property-data-table"}>
+                            <tbody>
+                            <tr>
+                                <th>Address</th>
+                                <td>{property.city}, {property.street}, {property.house_number}</td>
+                            </tr>
+                            <tr>
+                                <th>Size</th>
+                                <td>{property.size} m2</td>
+                            </tr>
+                            <tr>
+                                <th>Rooms</th>
+                                <td>{property.rooms}</td>
+                            </tr>
+                            <tr>
+                                <th>Bathrooms</th>
+                                <td>{property.bathroom_count}</td>
+                            </tr>
+                            <tr>
+                                <th>Stories</th>
+                                <td>{property.floor}</td>
+                            </tr>
+                            <tr>
+                                <th>Build material</th>
+                                <td>{property.building_material}</td>
+                            </tr>
+                            <tr>
+                                <th>Type</th>
+                                <td>{property.type}</td>
+                            </tr>
+                            <tr>
+                                <th>Plot size</th>
+                                <td>{property.plot_size}</td>
+                            </tr>
+                            <tr>
+                                <th>Garages</th>
+                                <td>{property.garage}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className={"col-span-1 property-owner apply-square-background"}>
+                    <h1>Property owner</h1>
+                    </div>
+                </div>
+            </div>
+
+            /*<div
                 className={'my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400"'}>
 
                 <h3 className="text-3xl  leading-tight">
@@ -37,7 +111,7 @@ function PropertyCard(props) {
                     >{property.size}</small
                     >
                 </h3>
-            </div>
+            </div>*/
         );
     }
 }
