3dda8e572cde16f43126ff02a965efe687e0a338
Bodis Patrik <bodis.uk@gmail.com>
2024-09-19 11:16:50 +0200

    message controller and service created, new message, and get all message created

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -16,8 +16,41 @@ class MessageController extends Controller
         $this->messageService = new MessageService();
     }
 
+    public function newMessage(Request $request){
+      $id_user_from = \auth('sanctum')->id();
+       $message = $this->messageService->newMessage($request, $id_user_from);
+
+       if($message){
+           return response()->json([
+               'success' => true,
+               'message' => $message,
+           ],200);
+       }else{
+           return response()->json([
+               'success' => false,
+           ],412);
+       }
+
+    }
     public function getMessages(){
         $userID = \auth('sanctum')->id();
-        return response()->json(["hello", $userID]);
+       $messages[] = $this->messageService->getAllMessage($userID);
+       if($messages){
+           return response()->json([
+               'success' => true,
+               'messages' => $messages,
+           ],200);
+       }else{
+           return response()->json([
+               'success' => false,
+               'messages' => 'No messages',
+           ],404);
+       }
     }
+
+    public function answerMessage(Request $request){
+        $userID = \auth('sanctum')->id();
+        $message = $this->messageService->answerMessage($request, $userID);
+    }
+
 }
--- backend/app/Services/MessageService.php
+++ backend/app/Services/MessageService.php
@@ -2,7 +2,29 @@
 
 namespace App\Services;
 
+use Illuminate\Http\Request;
+use App\Models\Message;
 class MessageService
 {
+    public function newMessage(Request $request, $id){
+        $request->validate([
+            'id_user_to' => 'required',
+            'title' => 'required|max:255',
+            'message' => 'required',
+            'id_property'=> 'required'
+        ]);
 
+        $message = Message::create([
+            'id_user_from'=>$id,
+            'id_user_to'=>$request->id_user_to,
+            'id_property'=>$request->id_property,
+            'title'=>$request->title,
+            'message'=>$request->message
+        ]);
+        return $message;
+    }
+
+    public function getAllMessage($id_user_to){
+       return Message::all()->where('id_user_to',$id_user_to);
+    }
 }
